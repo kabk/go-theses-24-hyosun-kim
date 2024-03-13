0845db8515c1449d9ffc5645cf9da38f729e7de6
Lucky Hyosun 효선킴 <73243458+HellobonjourLucky@users.noreply.github.com>
2024-03-13 19:33:55 +0100

    last update!

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
 st11s.forEach(st11 => {
     st11.addEventListener('mouseover', function(e){
